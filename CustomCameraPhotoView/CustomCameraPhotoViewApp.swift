5c8e48bb1eedbcf2a19faf1e246265df86c40f1a
ljy0932@nate.com <reborn@rebornui-Macmini.local>
2023-03-30 11:09:42 +0900

    d

---
+++ CustomCameraPhotoView/CustomCameraPhotoViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraPhotoViewApp.swift
+//  CustomCameraPhotoView
+//
+//  Created by reborn on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraPhotoViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CustomCameraPhotoView()
+        }
+    }
+}
